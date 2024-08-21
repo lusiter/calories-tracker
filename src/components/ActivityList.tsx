bc8e357e425d49a85eca6b41e63f4aab9a63f0fb
lusiter <67213287+lusiter@users.noreply.github.com>
2024-08-20 21:45:09 -0500

    Primer commit de proyecto de calorias y ejercicios

---
+++ src/components/ActivityList.tsx
@@ -0,0 +1,69 @@
+import {Activity} from "../types"
+import { categories } from "../data/category"
+import { useMemo, Dispatch } from "react"
+import {PencilSquareIcon, TrashIcon} from '@heroicons/react/24/outline'
+import { ActivityActions } from "../reducers/activity-reducer"
+ 
+type ActivityListProps = {
+    activities: Activity[],
+    dispatch: Dispatch<ActivityActions>
+}
+
+function ActivityList({activities, dispatch}: ActivityListProps) {
+     const categoryName = useMemo(()=>
+    (category: Activity['category']) => categories.map( cat => cat.id === category ? cat.name : '')
+     , [activities]
+    )
+const isEmptyActivies = useMemo(() => activities.length == 0, [activities])
+
+    return (
+    
+    <>
+    <h2 className="text-4xl font-bold text-slate-600 text-center">Comida Y Actividades</h2>
+
+    {isEmptyActivies ?
+    <p className="text-center">No hay actividades.......</p>:
+    
+    activities.map( activity => (
+     <div key={activity.id} className="px-5 py-10 bg-stone-300 mt-5 flex justify-between">
+        <div className="space-y-2 relative">
+            <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
+                {categoryName(+activity.category)}
+            </p>
+            <p className="text-2xl font-bold pt-5">
+                {activity.name}
+            </p>
+            <p className="font-black text-4xl text-lime-500">{activity.calories} {''}
+                <span>Calorias</span>
+            </p>
+        </div>
+
+        <div className=" flex gap-5 items-center">
+               <button
+               onClick={() => dispatch({type: "set-activeId", payload: {id: activity.id}})}
+               >
+                <PencilSquareIcon className="h-7 w-7 text-gray-800"
+
+                />
+               </button>
+               <button 
+               onClick={() => dispatch({type: "delete-activeId", payload: {id: activity.id}})}>
+                <TrashIcon className="h-7 w-7 text-gray-800"
+
+                />
+               </button>
+               
+        </div>
+
+
+     </div>
+    ))}    
+
+
+
+
+    </>
+  )
+}
+
+export default ActivityList
